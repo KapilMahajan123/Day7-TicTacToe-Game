8733e04343557ecea52e704a8579e545d052a0e0
kapilmahajan123 <montyempire123gmail.com>
2022-12-06 20:26:10 +0530

    Added: Check Winner in every move

--- src/com/TicTacToe/java/TicTacToe.java
+++ src/com/TicTacToe/java/TicTacToe.java
@@ -9,110 +9,164 @@ import java.util.*;
  */
 public class TicTacToe {
 	
-	static char[] board = new char[10];  //taking array
-	static char player, computer;
-	private static int playLocation;
-	public static Scanner scannerObject = new Scanner(System.in); //creating scanner object
-	
-	    /*
-	     *creating the game board
-	     *Select the index from 1 to 9 to make the move.
-	     */
-		 public static void createBoard()
-		 {
-	        for (int index = 1; index < 10; index++)      //Initalize
-	        {
-	            board[index] = ' ';
-	        }
-	    }
-	 
-	    /*
-	     * This game has two player x and o
-	     */
-		 public static void getPlayerChoice()
-		 {
+	static char[] board = new char[10];    //Taking array
+	static char turn;
 
-			 System.out.print("select X or O : ");
-			 player = Character.toUpperCase(scannerObject.next().charAt(0));
+	/**
+	 * Creating the game board
+	 * Select the index from 1 to 9 to make the movw
+	 */
+	private static void createBoard() {
+		System.out.println("Welcome to Tic Tac Toe");
+		for (int i = 1; i < 10; i++) {		//initialize
+			board[i] = ' ';
+		}
+	}
 
-			 	if (player == 'X')
-	            computer = 'O';
-			 	else
-	            computer = 'X';
-			 	System.out.println("You have selected : " + player);
-			 	System.out.println("Computer's choice is : " + computer);
-		 }	
-	    
-		 	/*
-		 	 * Displaying the tictactoe game board
-		 	 */
-		 	 public static void showBoard()
-		 	 {
-		 		 System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
-		 		 System.out.println("---------");
-		 		 System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
-		 		 System.out.println("---------");
-		 		 System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-		 	 }
-	    
-		 	 /*
-		 	  * user is going to make a move to the desired location
-		 	  * if it is not in range from 1 to 9
-		 	  * then it will show you an invalid input
-		 	  */
-		 	 public static void userMove()
-		 	 {
-		 		 System.out.println("Enter Location 1-9 to Make Move");
-		 		 playLocation = scannerObject.nextInt();
-		 		 if (playLocation < 10 && playLocation > 0)
-		 		 {
-		 			 board[playLocation] = player;
-		 			 showBoard();
-		 		 } else
-		 		 {
-		 			 System.out.println("Invalid Choice");
-		 		 }
-		 	 }
-	     
-		 	 /*
-		 	  * the isEmpty is an inbuilt method 
-		 	  * boolean value is returned by the isEmpty method
-		 	  * Check whether is the given string empty or not 
-		 	  */
-		 	 public static boolean isEmpty() {
-		       if (board[playLocation] == ' ') {
-		            return true;      
-		       } else {
-		            return false;
-		       }
-	       }
-		 	 /*
-		 	  * Tossing the coin to checking who won the first
-		 	  */
-		 	 public static void checkToss() {
-		 		 Random random = new Random();
-		 		 int tossResult = random.nextInt(2)+1;
-		 		 System.out.println("\nChoose 1 for Heads or 2 for Tails");
-		 		 int coinSelect = scannerObject.nextInt();
-		 		 
-		 		 if (coinSelect == tossResult) {
-		 			 System.out.println("\nPlayer Won The Toss! Player Starts");
-		 		 }else {
-		 			System.out.println("\nComputer Won The Toss! Computer Starts");
-		 		 }
-		 	 }
-		 	 
+	/**
+	 * This game has two player x and o
+	 * taking input from user to choose the turn
+	 */
+	private static void chooseLetter() {
+		Scanner scanner = new Scanner(System.in);  // Create an object
+		System.out.println("Enter the character X or O to choose the turn:");
+		char turn = scanner.next().charAt(0);
+		if (turn == 'X' || turn == 'x') {
+			System.out.println("Computer's turn to play the game");
+			turn = 'O';
+		} else if (turn == 'O' || turn == 'o') {
+			System.out.println("Player's turn to play the game");
+			turn = 'X';
+		} else {
+			System.out.println("Invalid input");
+		}
 
-	    public static void main(String[] args)
-	    {
+	}
 
-	        System.out.println("Welcome To Tic Tac Toe");
-	        checkToss();
-	        createBoard();
-	        getPlayerChoice();
-	        showBoard();
-	        userMove();
-	        isEmpty();
+	/**
+	 * Displaying the tictactoe game board
+	 */
+	private static void showBoard() {
+		
+		
+		 System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+ 		 System.out.println("---------");
+ 		 System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+ 		 System.out.println("---------");
+ 		 System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 
-	    }
+	}
+
+	/**
+	 * user has already made a move
+	 * if it is not in range from 1 to 9
+	 * then it will show you an invalid input and then re-entering number
+	 */
+	private static void madeMove(boolean firstPlayer) {
+		Scanner scanner = new Scanner(System.in);
+		String winner = null;
+		if (firstPlayer == true) {
+			turn = 'X';
+		} else {
+			turn = 'O';
+		}
+		while (winner == null) {
+			System.out.println("Enter your slot number from 1 to 9: ");
+			int move = scanner.nextInt();
+			if (!(move > 0) && (move < 10)) {
+				System.out.println("Invalid move, Re-enter the slot number:");
+				continue;
+			} else if (board[move] == ' ') {
+				board[move] = turn;
+				showBoard();
+				if (turn == 'X') {
+					turn = 'O';
+				} else {
+					turn = 'X';
+				}
+				winner = checkWiningCombinations();
+
+			} else {
+				System.out.println("Slot is taken already; Re-enter the slot number: ");
+				showBoard();
+				continue;
+			}
+		}
+
+		if (winner.equalsIgnoreCase("tie")) {
+			System.out.println("It's a tie| Thanks for playing");
+		} else {
+			System.out.println("Congratulations! " + winner + " has won.");
+		}
+	}
+
+	/**
+	 * Tossing the coin to check who plays first
+	 */
+	private static boolean tossToStartGame() {
+		Random random = new Random();
+		return random.nextBoolean();
+	}
+
+	/**
+	 * Checking after move the winner or the tie or change in turn
+	 */
+	private static String checkWiningCombinations() {
+		for (int a = 1; a < 9; a++) {
+			StringBuilder sb = new StringBuilder();
+			String line;
+
+			switch (a) {
+			case 1:
+				line = Character.toString(board[1] + board[2] + board[3]);
+				break;
+			case 2:
+				line = Character.toString(board[4] + board[5] + board[6]);
+				break;
+			case 3:
+				line = Character.toString(board[7] + board[8] + board[9]);
+				break;
+			case 4:
+				line = Character.toString(board[1] + board[4] + board[7]);
+				break;
+			case 5:
+				line = Character.toString(board[2] + board[5] + board[8]);
+				break;
+			case 6:
+				line = Character.toString(board[3] + board[6] + board[9]);
+				break;
+			case 7:
+				line = Character.toString(board[1] + board[5] + board[9]);
+				break;
+			case 8:
+				line = Character.toString(board[3] + board[5] + board[7]);
+				System.out.println("String is: " + line);
+				break;
+
+			}
+			if (sb.equals("XXX")) {
+				return "Computer";
+			} else if (sb.equals("OOO")) {
+				return "Player";
+			}
+		}
+		for (int i = 1; i < 10; i++) {
+			if (board[i] == ' ') {
+				break;
+			} else if (i == 9)
+				return "tie";
+
+		}
+		System.out.println(turn + "turn; enter a slot number to place the " + turn + " in: ");
+		return null;
+
+	}
+
+	public static void main(String args[]) {
+
+		createBoard();
+		chooseLetter();
+		showBoard();
+		madeMove(true);
+	}
 }
